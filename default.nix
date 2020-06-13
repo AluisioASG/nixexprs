2f1a3b865cae62eb557aafb2012c8eee8197a848
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-06-13 08:11:28 -0300

    Update callPackage with own packages
    Turns out pkgs.callPackage is bound to its original scope, and we need
    a new one with our packages included so they can be patched.

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { pkgs ? import <nixpkgs> { } }:
 let
+  withNewCallPackage = set: set // { callPackage = pkgs.lib.callPackageWith set; };
   myPkgs = import ./pkgs { inherit pkgs; };
-  myPatchedPkgs = import ./patches { pkgs = pkgs // myPkgs; };
+  myPatchedPkgs = import ./patches { pkgs = withNewCallPackage (pkgs // myPkgs); };
 in
 {
   modules = import ./modules;
