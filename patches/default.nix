0b021297578a635bdf65f31b96e02ff38125a319
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-08-10 08:47:33 -0300

    patches/ipfs-cluster: Patch to enable connecting to IPFS via Unix socket

--- patches/default.nix
+++ patches/default.nix
@@ -6,6 +6,8 @@ let
 
     haunt = callPackage ./haunt { };
 
+    ipfs-cluster = callPackage ./ipfs-cluster { };
+
   };
 in
 self
--- patches/overlay.nix
+++ patches/overlay.nix
@@ -4,4 +4,6 @@ self: super:
 
   haunt = import ./haunt { inherit (super) haunt; };
 
+  ipfs-cluster = import ./ipfs-cluster { inherit (super) ipfs-cluster; };
+
 }
