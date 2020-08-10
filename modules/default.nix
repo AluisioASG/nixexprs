483b4f0e108e6040bb72952395dfadc792ae97a2
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-08-10 07:40:14 -0300

    modules/ipfs-cluster: init

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,6 @@
 {
-  postgresql-base-backup = ./services/databases/postgresql-base-backup.nix;
   dma = ./programs/dma.nix;
   dyndnsc = ./services/networking/dyndnsc.nix;
+  ipfs-cluster = ./services/cluster/ipfs-cluster.nix;
+  postgresql-base-backup = ./services/databases/postgresql-base-backup.nix;
 }
