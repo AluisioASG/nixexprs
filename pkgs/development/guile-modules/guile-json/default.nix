06d83e354c9e8ed7725daafc370a196fb0fde626
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-09-13 07:44:49 +0000

    guile-json: 4.3.0 -> 4.3.2

--- pkgs/development/guile-modules/guile-json/default.nix
+++ pkgs/development/guile-modules/guile-json/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "guile-json";
-  version = "4.3.0";
+  version = "4.3.2";
 
   src = builtins.fetchTarball {
     url = "https://download.savannah.nongnu.org/releases/guile-json/guile-json-${version}.tar.gz";
-    sha256 = "1daqswls05z7nlhvcwi32agh6h94jk7smn6liw4vrigwiqiz0cg2";
+    sha256 = "1ciqx3zhyl703lv7s2whi860lq9455z56c5w26zvx6fdlblgz84p";
   };
 
   nativeBuildInputs = [ pkgconfig ];
