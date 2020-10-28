6aaa18bb190a9d426677fd1c37cf9bfd55de263c
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-10-28 17:49:36 -0300

    Add experimental flake support

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "aasg's Nix expressions";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = { self, flake-utils, nixpkgs }:
+    let
+      inherit (flake-utils.lib) defaultSystems flattenTree;
+      inherit (nixpkgs.lib.attrsets) filterAttrs genAttrs mapAttrs;
+      inherit (nixpkgs.lib.strings) hasPrefix hasSuffix;
+      inherit (nixpkgs.lib.trivial) id pipe;
+    in
+    {
+      #lib = import ./lib { inherit (nixpkgs) lib; };
+      packages = genAttrs defaultSystems
+        (system: pipe (import ./pkgs { pkgs = nixpkgs.legacyPackages.${system}; }) [
+          # Filter out linuxPackages before it gets evaluated.
+          (if ! hasSuffix "linux" system
+          then filterAttrs (attr: drv: ! hasPrefix "linuxPackages" attr)
+          else id)
+          # Flatten package sets.
+          flattenTree
+          # Remove packages not compatible with this system.
+          (filterAttrs (attr: drv: builtins.elem system drv.meta.platforms))
+        ]);
+      nixosModules = mapAttrs (name: path: import path) (import ./modules);
+      overlays = {
+        pkgs = import ./pkgs/overlay.nix;
+        patches = import ./patches/overlay.nix;
+      };
+    };
+}
