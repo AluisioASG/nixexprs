6daf3168e28b9644d410f314b6fb46f3935a8d70
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-11-04 16:00:56 -0300

    drep: Depend on CoreServices on macOS

--- pkgs/tools/text/drep/default.nix
+++ pkgs/tools/text/drep/default.nix
@@ -1,9 +1,10 @@
-{ lib, callPackage, defaultCrateOverrides }:
+{ stdenv, callPackage, defaultCrateOverrides, CoreServices }:
 
 (callPackage ./Cargo.nix { }).workspaceMembers.drep.build.override {
   crateOverrides = defaultCrateOverrides // {
     drep = attrs: {
-      meta = with lib; {
+      buildInputs = stdenv.lib.optional stdenv.isDarwin CoreServices;
+      meta = with stdenv.lib; {
         description = "A grep with runtime reloadable filters";
         homepage = "https://github.com/maxpert/drep";
         license = licenses.mit;
